e1c1436112ea0cfd42b4cfb2aec4e3b8e8581e27
mako <o2ka.m328@gmail.com>
2024-11-02 11:30:13 +0000

    profile login link

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,13 @@ Route::get('/', function () {
 
 use App\Http\Controllers\Admin\NewsController;
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add');
+    Route::get('news/create', 'add')->middleware('auth');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add');
-    Route::get('prefile/edit', 'edit');
+    Route::get('profile/create', 'add')->middleware('auth');
+    Route::get('prefile/edit', 'edit')->middleware('auth');
 });
 
 
