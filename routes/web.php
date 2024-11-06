f8f9848b6975e606f080a11cb505328b0fabcd9f
mako <o2ka.m328@gmail.com>
2024-11-06 13:28:09 +0000

    my profile form

--- routes/web.php
+++ routes/web.php
@@ -18,28 +18,21 @@ Route::get('/', function () {
 });
 
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-    Route::get('news/create', 'add')->middleware('auth');
+
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('news/create', 'add')->name('news.add');
+    Route::post('news/create', 'create')->name('news.create');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
-Route::controller(ProfileController::class)->prefix('admin')->group(function() {
-    Route::get('profile/create', 'add')->middleware('auth');
-    Route::get('prefile/edit', 'edit')->middleware('auth');
+
+Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('profile/create', 'add')->name('profile.add');
+    Route::post('profile/create', 'create')->name('profile.create');
+    Route::get('prefile/edit', 'edit')->name('profile.edit');
+    Route::post('profile/update', 'update')->name('profile.update');
 });
 
-
-/*
-|
-|課題3.
-|http://XXXXXX.jp/XXX というアクセスが来たときに、
-|AAAControllerのbbbというAction に渡すRoutingの設定
-|
-|Route::controller(AAA::class)->group(function(){
-|    Route::get('XXX','bbb');
-|});
-|
-*/
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
