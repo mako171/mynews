cf40cdca585ae00e9de70ea12803e0154d019966
mako <o2ka.m328@gmail.com>
2024-12-25 12:45:23 +0000

    番 環境用修正しました

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <div class="container">
     <hr color="#c0c0c0">
+    <h2>ニュース</h2>
     @if (!is_null($headline))
     <div class="row">
         <div class="headline col-md-10 mx-auto">
